4b2c899c1e620b74e3c35dc1e7e5e6cce97ec729
Rashidalabri0803 <rashid92759346@gmail.com>
2024-10-19 01:22:25 +0400

    s

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    webbrowser.open('http://0.0.0.0:8000')
+    webbrowser.open('http://localhost:8000')
     execute_from_command_line(sys.argv + ['runserver', '0.0.0.0:8000', '--noreload'])
 
 
